f169e96675ac0532929ac9f254654eca26b8f771
polosa00 <i@apolochanin.ru>
2023-05-17 20:04:14 +0300

    Initial Commit

---
+++ TaskListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskListApp
+//
+//  Created by Александр Полочанин on 17.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
